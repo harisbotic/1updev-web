e387d0ae2cd34b60f7d9930fffdd7c2fa6506891
Amila <aborancic1@etf.unsa.ba>
2019-08-26 19:06:09 +0200

    created mock data for ranking and activity logs

---
+++ src/api/activity_mock.js
@@ -0,0 +1,25 @@
+import images from "../assets/imageExports"
+
+export const mockActivity = [
+    {
+        user: "random dudebro",
+        profileImg: images.randomguy,
+        itemPrice: "125 Tokens",
+        itemName: "Item 1",
+        purchaseTime: "04:20am"
+    },
+    {
+        user: "random girl",
+        profileImg: images.randomgal,
+        itemPrice: "50 Tokens",
+        itemName: "Item 50",
+        purchaseTime: "14:17pm"
+    },
+    {
+        user: "test test",
+        profileImg: images.randomguy,
+        itemPrice: "50 Tokens",
+        itemName: "Item ABC",
+        purchaseTime: "22:34pm"
+    }
+]
