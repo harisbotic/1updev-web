27908f1523683d1f334a4d66fb5c6c273f6b40a8
Almir <almir.mulalic.am@gmail.com>
2019-08-30 04:46:29 +0200

    improved filters,added hover on item, imporvments on design and responsivnes,implemented disenchant and activate

--- src/app/Profile/Components/Item/Item.component.js
+++ src/app/Profile/Components/Item/Item.component.js
@@ -8,25 +8,98 @@ export class Item extends Component {
         super();
 
         this.state = {
-            isHovered : false
+            isHovered : false,
+            hoveredButton:""
         }
     }
 
     toggleHover(state){
         this.setState({'isHovered':!this.state.isHovered});
     }
-   
 
-      
+    switchHoverInfo(hoveredButton) {
+    
+        switch(hoveredButton) {
+            case 'activate':
+                return( 
+                    <div className="hoverText">
+                        <p>Activate price</p>
+                        <p>{this.props.itemActivatePrice} Tokens</p>
+                    </div>
+                );
+            case 'gift':
+                return( 
+                    <div className="hoverText">
+                        <p>Gifting is</p>
+                        <p>free :D</p>
+                    </div>
+                );
+            case 'disenchant':
+                return( 
+                    <div className="hoverText">
+                        <p>Disenchant value</p>
+                        <p>{this.props.itemDisenchantValue} Tokens</p>
+                    </div>
+                );
+            default:
+                    return( 
+                        <div className="hoverText">
+                        <p>.</p>
+                        <p>.</p>
+                        </div>
+                    );
+      }
+
+    }
+   
+    
     render() {
+
+        const isHovered = this.state.isHovered;
+        const hoveredButton = this.state.hoveredButton;
+
         return (
         
             <div className="item-card" style={{background:this.props.background}} onMouseEnter = {()=>this.toggleHover(this.state)} onMouseLeave = {()=>this.toggleHover(this.state)}>
-                <p className="itemCategory">{this.props.itemCategory}</p>
-                <i className={this.props.itemIcon}></i>
-                <p className="itemName">{this.props.itemName}</p>
-                <p className="itemValue">{this.props.itemValue} Tokens</p>
-                <p className="itemType">{this.props.itemType}</p>
+            { !isHovered ? (
+                <div>
+                    <p className="itemCategory">{this.props.itemCategory}</p>
+                    <i className={this.props.itemIcon}></i>
+                    <p className="itemName">{this.props.itemName}</p>
+                    <p className="itemValue">{this.props.itemValue} Tokens</p>
+                    <p className="itemType">{this.props.itemType}</p>
+                </div>
+                ) : (
+                    
+                    <div className="hoveredState">
+                        <div 
+                            className="editProfileButton" 
+                            onMouseEnter = {()=>this.setState({"hoveredButton":"activate"})} 
+                            onMouseLeave = {()=>this.setState({"hoveredButton":""})}
+                            onClick = {() => {this.props.disenchantItem(this.props.itemId); this.setState({"isHovered":true}); }}
+                        >    
+                            <p>ACTIVATE</p>
+                        </div>
+                        <div 
+                            className="editProfileButton" 
+                            onMouseEnter = {()=>this.setState({"hoveredButton":"gift"})} 
+                            onMouseLeave = {()=>this.setState({"hoveredButton":""})}
+                        >    
+                            <p>GIFT</p>
+                        </div>
+                        <div 
+                            className="editProfileButton" 
+                            onMouseEnter = {()=>this.setState({"hoveredButton":"disenchant"})} 
+                            onMouseLeave = {()=>this.setState({"hoveredButton":""})}
+                            onClick = {() => {this.props.disenchantItem(this.props.itemId)}}
+                        >    
+                            <p>DISENCHANT</p>
+                        </div>
+                    </div>
+                )}
+
+                {this.switchHoverInfo(hoveredButton)}
+                    
             </div>
         )
     }
--- src/app/Profile/Profile.js
+++ src/app/Profile/Profile.js
@@ -1,11 +1,7 @@
 import React, { Component } from "react";
-// import Button from 'react-bootstrap/Button';
 
-import Item from './Components/Item/Item.component'
-import ActivityLog from './Components/ActivityLog/ActivityLog.js'
-
-
-//import Inventory from './Components/Inventory/inventory.component.js';
+import Item from './Components/Item/Item.component';
+import ActivityLog from './Components/ActivityLog/ActivityLog.js';
 
 import "./Profile.scss";
 
@@ -22,17 +18,17 @@ export class Profile extends Component {
         super();
 
         this.state = {
-            itemList: jsonItemList.items,
+            itemList: jsonItemList,
             searchField: "",
             profile: jsonProfileList,
+            isAscending:true,
             filteredList: jsonItemList.items
         }
-
     }
 
     searchFilterInventory = (searchText) => {
 
-        const itemList = this.state.itemList;
+        const itemList = this.state.itemList.items;
 
         this.setState({
             "filteredList":
@@ -44,35 +40,26 @@ export class Profile extends Component {
     typeFilter = (target) => {
 
         const filteredList = this.state.filteredList;
+        const isAscending = this.state.isAscending;
 
-        switch (target) {
-            case "name":
-                this.setState({
-                    "filteredList":
-                        filteredList.sort((a, b) => a.name.localeCompare(b.name))
-                })
-                break
-            case "value":
-                this.setState({
-                    "filteredList":
-                        filteredList.sort((a, b) => a.value.toString().localeCompare(b.value.toString(), undefined, { numeric: true }))
-                })
-                break
-            case "category":
-                this.setState({
-                    "filteredList":
-                        filteredList.sort((a, b) => a.category.localeCompare(b.category))
-                })
-                break
-            default:
-
+        if(isAscending) {
+            this.setState({
+                "filteredList":
+                    filteredList.sort((a, b) => a[target].toString().localeCompare(b[target].toString(), undefined, { numeric: (target === "value") }))
+            })   
+        } else if(!isAscending) {
+            this.setState({
+                "filteredList":
+                    filteredList.sort((a, b) => b[target].toString().localeCompare(a[target].toString(), undefined, { numeric: target === "value" }))
+            }) 
         }
-
+        
+        this.setState({"isAscending":!isAscending});
     }
 
     getBadges = () => {
 
-        this.state.filteredList.items.map((item) => {
+        jsonItemList.map((item) => {
             return (
                 <div className="badge">
                     <i className="fa fas-trophy"></i>
@@ -81,10 +68,19 @@ export class Profile extends Component {
         })
     }
 
+    disenchantItem = (id) => {
+
+        const filteredList = this.state.filteredList.filter(item => item.id !== id)
+
+        {/* LOGIC TO REMOVE IT FROM DATABASE */}
+
+        this.setState({"filteredList":filteredList});
+
+    }
 
     render() {
 
-        const itemList = this.state.itemList;
+        const itemList = this.state.itemList.items;
         const filteredList = this.state.filteredList;
 
         return (
@@ -92,59 +88,63 @@ export class Profile extends Component {
 
                 <div className="profileDisplayComponent">
 
-                    <div className="profileDisplay">
+                    <img className="profilePicture" src="https://icon-library.net/images/default-user-icon/default-user-icon-4.jpg" alt="user" />
 
-                        <img className="profilePicture" src="https://icon-library.net/images/default-user-icon/default-user-icon-4.jpg" alt="user" />
+                    <div className="profileDetails">
+            
+                        <div className="editProfileButton"><p>EDIT PROFILE</p></div>
 
-                        <div className="profileDetails">
+                        <p className="userTitle">{this.state.title ?
+                            (this.state.title)
+                            : ("User special title goes here")}
+                        </p>
 
-                            {/* <Button variant="info">Edit Profile</Button>*/}
+                        <h1 className="username">@{this.state.profile.identity.username}</h1>
 
-                            <p className="userTitle">{this.state.title ?
-                                (this.state.title)
-                                : ("User special title goes here")}
-                            </p>
-
-                            <h1 className="username">@{this.state.profile.identity.username}</h1>
-
-                            <div className="tokenRelated">
-                                <p className="availableTokens">Available tokens: {this.state.availableTokens}</p>
-                                <p className="profileValue">Profile value: {this.state.availableTokens}</p>
-                                <p className="profileRank">#6</p>
-                            </div>
-
-                            <div className="badges">
-                                {
-                                    itemList.map(item => {
-                                        if (item.category === "Badge" && item.isActive)
-                                            return (
-                                                <div key={item.id} className="badge">
-                                                    <i className={item.icon}></i>
-                                                </div>
-                                            )
-                                        else return null;
-                                    })
-                                }
-                            </div>
+                        <div className="tokenRelated">
+                            <p className="availableTokens">Available tokens: {this.state.availableTokens}</p>
+                            <p className="profileValue">Profile value: {this.state.availableTokens}</p>
+                            <p className="profileRank">#6</p>
                         </div>
-                    </div>
+
+                        <div className="badges">
+                            {
+                                itemList.map(item => {  
+                                    if (item.category === "Badge" && item.isActive)
+                                        return (
+                                            <div key={item.id} className="badge">
+                                            <i className={item.icon}></i>
+                                            </div>
+                                        )
+                                    else return null;
+                                })
+                            }
+                        </div>
+                    
+                        </div>
+
+                    <ActivityLog className="activityLog" />
+
                 </div>
-
-                <ActivityLog className="activityLog" />
-
+            
                 <div className="inventoryContainer">
+                    
                     <div className="infoSection">
-                        <h1 className="heading">
-                            <span className="inventoryText">Inventory</span>
-                            <span className="inventoryValue">(INVENTORY VALUE:
-                    <span className="tokenValue"> 1250</span> Tokens)
-                </span>
-                        </h1>
+                    
+                    <div className="infoText">
+                        <p className="inventoryText">Inventory</p>
+                        <p className="inventoryValue">(INVENTORY VALUE:  
+                            <span className="tokenValue"> {jsonProfileList.identity.profile.tokenValue} </span>
+                        Tokens)</p>
+                    </div>
+                        
+                        
                         <div className="filterOptions">
-                            <p>Sort by : </p>
+                            <p>FILTER</p>
                             <p id="sortByName" onClick={() => this.typeFilter("name")}>Name <i className="fas fa-caret-down"></i></p>
                             <p id="sortByValue" onClick={() => this.typeFilter("value")}>Value<i className="fas fa-caret-down"></i></p>
                             <p id="sortByCategory" onClick={() => this.typeFilter("category")}>Category <i className="fas fa-caret-down"></i></p>
+                           
                             <div className="searchBoxComponent">
                                 <i className="fas fa-search"></i>
                                 <input
@@ -155,8 +155,11 @@ export class Profile extends Component {
                                     placeholder="Search items..."
                                 />
                             </div>
+                        
                         </div>
+
                     </div>
+
                     <div className="itemsContainer">
                         {
                             filteredList.map((item) => {
@@ -164,12 +167,16 @@ export class Profile extends Component {
                                 return (
                                     <Item
                                         key={item.id}
+                                        itemId={item.id}
                                         background={item.background}
                                         itemCategory={item.category}
                                         itemIcon={item.icon}
                                         itemName={item.name}
                                         itemValue={item.value}
                                         itemType={item.type}
+                                        itemDisenchantValue={item.disenchantValue}
+                                        itemActivatePrice={item.activatePrice}
+                                        disenchantItem = {this.disenchantItem}
                                     />
 
                                 )
