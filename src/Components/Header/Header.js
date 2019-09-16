f0ec07b60871bf46ad81b090cbd069f96160136c
Edis Sijaric <edis.sijaric95@gmail.com>
2019-09-16 14:36:31 +0200

    Search menu funkcionalan i skoro pa izgledan

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -27,24 +27,6 @@ function Header (props) {
         })
     }
 
-    const DisplayDataHandler = () => {
-        // Declare variables
-        var input, filter, ul, li, a, i;
-        input = document.getElementById("mySearch");
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("myMenu");
-        li = ul.getElementsByTagName("li");
-      
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) {
-          a = li[i].getElementsByTagName("a")[0];
-          if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-          } else {
-            li[i].style.display = "none";
-          }
-        }
-      }
 
     return (
         <div className="container-fluid" id="header-container">
@@ -71,14 +53,15 @@ function Header (props) {
                 </div>
 
                 <div  className="col rightWrapper search" style={{ color: "white" }}>
-                    <input type="text" id="mySearch" placeholder="search user" onChange={onChangeHandler} onKeyUp={DisplayDataHandler} />
-                    <ul id="myMenu">
-                    {userList.userList.map((user, index) => {
-                        return (
-                            <li> {user.firstName} {user.lastName} </li>
-                        )
-                    })}
-                    </ul>
+                    <input type="text" id="mySearch" placeholder="search users" onChange={onChangeHandler} />
+                        <ul id="myMenu">
+                        {userList.userList.map((user, index) => {
+                            return (
+                                <li> {user.firstName} {user.lastName} </li>
+                            )
+                        })}
+                        </ul>
+                    
                 </div>
 
                 <div className="col rightWrapper">
